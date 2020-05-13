14e567d99e750cec8b189520bc81933fd9fb4119
Gonzalo Gutierrez <gutimore@gmail.com>
2020-05-13 20:13:08 -0300

    echo var for debugging

--- code/db_connection.php
+++ code/db_connection.php
@@ -1,4 +1,5 @@
 <?php
+echo $_ENV['MYSQLCONNSTR_connstring'];
 parse_str($_ENV['MYSQLCONNSTR_connstring'], $conn_str);
 // Create connection
 $conn = mysqli_connect($conn_str['servername'], $conn_str['username'], $conn_str['password'], $conn_str['db_name']);
